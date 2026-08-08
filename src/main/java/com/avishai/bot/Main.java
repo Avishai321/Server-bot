856a87112febfc5737dda5266b87904963d3af6e
Avishai <am303212575@gmail.com>
2026-08-08 21:10:05 +0300

    initial commit

--- src/main/java/com/avishai/bot/Main.java
+++ src/main/java/com/avishai/bot/Main.java
@@ -2,6 +2,12 @@ package com.avishai.bot;
 
 public class Main {
     public static void main(String[] args) {
+        String botUsername = "HomeServerManager_bot";
+        String botToken = System.getenv("BOT_TOKEN");
 
+        if (botToken == null || botToken.isEmpty()) {
+            System.err.println("Could not load bot token");
+            System.exit(1);
+        }
     }
 }
